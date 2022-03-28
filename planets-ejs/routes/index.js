03aa8850e936608163016c81d93449faa0f9f905
Emir <temindarov.e.i.20@gmail.com>
2022-03-28 19:24:07 +0300

    МАРШРУТИЗАТОР

--- planets-ejs/routes/index.js
+++ planets-ejs/routes/index.js
@@ -6,4 +6,29 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Страница Меркурия */
+router.get('/mercury', function(req, res, next) {
+  res.send("<h1>Страница Меркурия</h1>")
+});
+
+/* Страница Венеры */
+router.get('/venera', function(req, res, next) {
+  res.send("<h1>Страница Венеры</h1>")
+});
+
+/* Страница Земли */
+router.get('/earth', function(req, res, next) {
+  res.send("<h1>Страница Земли</h1>")
+});
+
+/* Страница Марса */
+router.get('/mars', function(req, res, next) {
+  res.send("<h1>Страница Марса</h1>")
+});
+
+/* Страница Юпитера */
+router.get('/jupiter', function(req, res, next) {
+  res.send("<h1>Страница Юпитера</h1>")
+});
+
 module.exports = router;
