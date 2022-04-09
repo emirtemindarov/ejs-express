9c7ea24a83f657e0d833de5c377878226e0af50b
Emir <temindarov.e.i.20@gmail.com>
2022-04-09 11:30:04 +0300

    ШАБЛОН SERIES

--- planets-ejs/file.js
+++ planets-ejs/file.js
@@ -1,21 +1,22 @@
+
 var async = require("async")
 
-var from_one_two_five = [1,2,3,4,5]
+async.series([
+        firstWord,
+        secondWord,
+        thirdWord
+    ],
+    function(err,result){
+        if(err) throw err;
+        console.log(result.join(' '))
+    });
 
-async.each(
-    from_one_two_five,
-    function(elem,report){
-        if(typeof elem === "number"){
-            report()
-        } else {
-            report("Нашли не число")
-        }
-    },
-    function(info){
-        if(info){
-            console.log(info)
-        } else {
-            console.log("Проверка прошла успешно. Все элементы массива являются числами")
-        }
-    }
-)
+function firstWord(callback){
+    callback(null, 'МАМА')
+}
+function secondWord(callback){
+    callback(null, 'МЫЛА')
+}
+function thirdWord(callback){
+    callback(null, 'РАМУ')
+}
