0cdeb37c22c9972457eda17c84028e3963ac6174
Emir <temindarov.e.i.20@gmail.com>
2022-04-09 11:40:37 +0300

    УТИЛИТА APPLY

--- planets-ejs/file.js
+++ planets-ejs/file.js
@@ -1,15 +1,16 @@
 
 var async = require("async")
 
-async.waterfall([
-    function(callback){
-        callback(null, "МАМА", "МЫЛА","РАМУ")
-    },
-    function(arg1, arg2, arg3, callback){
-        callback(null, arg1 + ' ' + arg2 + ' ' + arg3)
-    }
+async.series([
+    async.apply(anyWord, "МАМА"),
+    async.apply(anyWord, "МЫЛА"),
+    async.apply(anyWord, "РАМУ")
 ],
-function(err, result){
-    if(err) throw err
-    console.log(result)
+function(err,result){
+    if(err) throw err;
+    console.log(result.join(' '))
 });
+
+function anyWord(arg, callback){
+callback(null, arg)
+}
