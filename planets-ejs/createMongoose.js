40e16d0fdd30683657f20f8fe68e96a12359efad
Emir <temindarov.e.i.20@gmail.com>
2022-04-09 12:48:27 +0300

    ЗАПОЛНЕНИЕ БАЗЫ ДАННЫХ С MONGOOSE

--- planets-ejs/createMongoose.js
+++ planets-ejs/createMongoose.js
@@ -1,13 +1,39 @@
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/planets-ejs')
 var Planet = require("./models/planet").Planet
+var async = require("async")
 
-var planet = new Planet({
-    title: "Меркурий",
-    nick: "mercury"
-})
-
-console.log(planet)
-planet.save(function(err, planet, affected){
-    console.log(planet.title)
+mongoose.connection.on("open",function(){
+    var db = mongoose.connection.db
+    db.dropDatabase(function(err){
+        if(err) throw err;
+        async.parallel([
+            function(callback){
+                var mercury = new Planet({nick:"mercury"})
+                mercury.save(function(err,mercury){
+                    callback(err,"mercury")
+                })
+            },
+            function(callback){
+                var venera = new Planet({nick:"venera"})
+                venera.save(function(err,venera){
+                    callback(err,"venera")
+                })
+            },
+            function(callback){
+                var earth = new Planet({nick:"earth"})
+                earth.save(function(err,earth){
+                    callback(err,"earth")
+                })
+            }
+        ],
+        function(err,result){
+            if(err){
+                console.log(err)
+            } else {
+                console.log("Успешно созданы планеты с никами: " + result.join(", "))
+            }
+            mongoose.disconnect()
+        })
+    })
 })
