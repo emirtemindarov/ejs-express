890fe51797577972c3450bc5796484ca940ea5c1
Emir <temindarov.e.i.20@gmail.com>
2022-04-09 10:07:07 +0300

    СОЗДАНИЕ МОДУЛЯ ДАННЫХ

--- planets-ejs/createDB.js
+++ planets-ejs/createDB.js
@@ -1,5 +1,5 @@
 var MongoClient = require("mongodb").MongoClient
-
+var data = require("./data.js").data
 
 MongoClient.connect('mongodb://localhost:27017', function (err, client) {
   if (err) throw err;
