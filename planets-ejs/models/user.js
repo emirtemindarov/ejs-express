350053a0e7c740dc9150efc5cea54f8db5578c87
Emir <temindarov.e.i.20@gmail.com>
2022-06-15 09:00:41 +0300

    СОЗДАНИЕ МОДЕЛИ USER

---
+++ planets-ejs/models/user.js
@@ -0,0 +1,41 @@
+var mongoose = require("mongoose")
+var crypto = require("crypto")
+
+var schemaUser = mongoose.Schema({
+    username:{
+        type: String,
+        unique: true,
+        required: true
+    },
+    hashedPassword:{
+        type: String,
+        required: true
+    },
+    salt:{
+        type: String,
+        required: true
+    },
+    created:{
+        type:Date,
+        default: Date.now
+    }
+})
+
+
+schemaUser.virtual("password").set(function(password){
+    this._purePassword = password
+    this.salt = Math.random() + ""
+    this.hashedPassword = this.encryptPassword(password)
+}).get(function(){
+    return this._purePassword
+})
+
+schemaUser.methods.encryptPassword = function(password){
+    return crypto.createHmac('sha1', this.salt).update(password).digest('hex')
+}
+
+
+
+
+User = mongoose.model("User",schemaUser)
+module.exports.User = User
